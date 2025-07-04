13a38cc5ab4a1f951f647255a7b43c19ea00daf2
Hafiz Muhammad Huzaifa Javid <168843439+huzaifajavid07@users.noreply.github.com>
2025-07-04 14:00:40 -0700

    Initial commit 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import logo from '../assets/svg.png';
+
 
 const Navbar = () => {
     return (
         <>
             <nav className='flex justify-around bg-violet-900 text-white py-2'>
                 <div className="logo flex items-center gap-3">
-                    <img src="/svg.png" className="h-8" alt="Flowbite Logo" />
+                    <img src={logo} className="h-8" alt="iTask Logo" />
                     <span className='font-bold text-xl'>iTask</span>
                 </div>
                 <ul className="flex gap-8 mx-9">
